a89668856126bc6272a32fca2900daceb9774333
kenser <koffy140221@gmail.com>
2023-06-05 23:14:55 -0600

    ultimas modificaciones

--- vistas/clientes/buscar.php
+++ vistas/clientes/buscar.php
@@ -7,7 +7,7 @@
                 <div class="row mb-3">
                     <div class="col">
                         <label for="cliente_nombre">Nombre del cliente</label>
-                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control" required>
+                        <input type="text" name="cliente_nombre" id="cliente_nombre" class="form-control">
                     </div>
                 </div>
                 <div class="row mb-3">
